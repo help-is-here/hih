31a4e159f47bfea406c7b471887e18bf6e760c6a
Abi <abigail@abigailhendrick.com>
2023-12-16 13:14:28 -0500

    Initial commit

---
+++ src/MiniTag.tsx
@@ -0,0 +1,7 @@
+type TagProps = {
+    children: React.ReactNode
+}
+
+export default function MiniTag({children}:TagProps){
+    return <div className="bg-orange-500 rounded-full text-white px-2 py-1">{children}</div>
+}
---
+++ src/Tag.tsx
@@ -0,0 +1,7 @@
+type TagProps = {
+    children: React.ReactNode
+}
+
+export default function Tag({children}:TagProps){
+    return <div className="bg-orange-900 rounded-full text-white px-4 py-2">{children}</div>
+}
