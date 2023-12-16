0698f11c0890fb3c31adf912b6183252aaab9a39
Abi <abigail@abigailhendrick.com>
2023-12-16 15:21:15 -0500

    Added supabase integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import './App.css'
 import {FaInfoCircle} from "react-icons/fa"
 import Tag from './Tag'
-import data from './data'
-import MiniTag from './MiniTag'
+import ResourceTable from './components/ResourceTable'
 
 function App() {
   return (
@@ -30,30 +29,7 @@ function App() {
       </div>
 
       <div className='px-24 py-12'>
-        <table className='w-full bg-white rounded-lg'>
-          <thead className='border border-solid border-0 border-b-8 border-orange-50 '>
-            <th className='text-left p-4'>Resource</th>
-            <th className='text-left p-4'>Description</th>
-            <th className='text-left p-4'>Tags</th>
-            <th></th>
-          </thead>
-          <tbody>
-            {data.map(d=>{
-              return (
-                <tr className='border-orange-50'>
-                  <td className='p-4'><a className="text-orange-900 underline" href={d.link}>{d.name}</a></td>
-                  <td className='p-4 w-96'>{d.description}</td>
-                  <td className='flex w-96 flex-wrap p-4 gap-1'>{d.tags.map(t=>{
-                    return (
-                      <MiniTag>{t.name}</MiniTag>
-                    )
-                  })}</td>
-                  <td className='text-xs p-4'>This resource helped <span className='text-lg bg-orange-200 rounded-full px-2 py-1'>{d.helped_num}</span>people</td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
+        <ResourceTable />
       </div>
     </div>
   )
---
+++ src/database/client.tsx
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js';
+import { Database } from './types';
+
+const client = createClient<Database>(import.meta.env.VITE_DB_URL, import.meta.env.VITE_DB_SECRET)
+export default client
