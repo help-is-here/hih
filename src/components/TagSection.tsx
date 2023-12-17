ffb922a2ac5d91305ff57a839035b05d1369fa0d
Xue <xue.contact@gmail.com>
2023-12-16 19:41:09 -0800

    Temporary fix to remove dependency from useEffect so it doesn't run constantly

--- src/components/TagSection.tsx
+++ src/components/TagSection.tsx
@@ -1,31 +1,35 @@
-import MiniTag from "@/MiniTag";
-import { useState, useEffect } from "react";
-import client from "@/database/client";
-import { QueryData } from "@supabase/supabase-js";
+import MiniTag from '@/MiniTag'
+import { useState, useEffect } from 'react'
+import client from '@/database/client'
+import { QueryData } from '@supabase/supabase-js'
 
 type TagSectionProps = {
     resourceId: number
 }
-export default function TagSection({resourceId}:TagSectionProps){
-    const tagsQuery = client.from("tag_resource").select("tags(name)").eq("resource_id", resourceId);
-    type TagsType = QueryData<typeof tagsQuery>;
-    const [tags, setTags] = useState<TagsType>([]);
+export default function TagSection({ resourceId }: TagSectionProps) {
+    const tagsQuery = client
+        .from('tag_resource')
+        .select('tags(name)')
+        .eq('resource_id', resourceId)
+    type TagsType = QueryData<typeof tagsQuery>
+    const [tags, setTags] = useState<TagsType>([])
 
-    useEffect(()=>{
-        const fetchData = async ()=>{
-            const { data, error } = await tagsQuery;
-            if (error) throw error;
-            const tags: TagsType = data;
+    useEffect(() => {
+        const fetchData = async () => {
+            const { data, error } = await tagsQuery
+            if (error) throw error
+            const tags: TagsType = data
             setTags(tags)
         }
-        fetchData();
-    }, [tagsQuery])
+        fetchData()
+    }, [])
 
     return (
-        <>{tags.map(tag=>{
-            // @ts-expect-error: Problem with typing in supabase. Hopefully will be fixed soon
-            return<MiniTag>{tag.tags.name}</MiniTag>
-        })}
+        <>
+            {tags.map((tag) => {
+                // @ts-expect-error: Problem with typing in supabase. Hopefully will be fixed soon
+                return <MiniTag>{tag.tags.name}</MiniTag>
+            })}
         </>
     )
 }
