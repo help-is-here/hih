ba5514647053d858dc71bb5139fb0e3d04e38996
Xue <xue.contact@gmail.com>
2023-12-17 12:39:14 -0800

    Fix padding bug on Explore All Resources

--- src/components/Home/CTA.tsx
+++ src/components/Home/CTA.tsx
@@ -3,7 +3,7 @@ export const CTA = () => {
         <section className="flex justify-center py-12 px-8">
             <a
                 href="/resources"
-                className="rounded-full bg-orange-500 text-white hover:bg-orange-600 px-12 py-8 md:text-4xl text-2xl "
+                className="rounded-full bg-orange-500 text-white text-center px-8 md:px-12 py-4 md:py-8 md:text-4xl text-2xl "
             >
                 Explore All Resources
             </a>
