8a2d00d74c18bdaed452f855c204ed72016ab5c6
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-12-18 13:13:18 -0800

    Install tailwind prettier plugin and editorconfig

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,11 +1,10 @@
 // prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
 
 /** @type {import("prettier").Config} */
-const config = {
+module.exports = {
+    plugins: ['prettier-plugin-tailwindcss'],
     trailingComma: 'es5',
     tabWidth: 4,
     semi: false,
     singleQuote: true,
 }
-
-export default config
