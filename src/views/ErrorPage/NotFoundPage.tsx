98ecfd85d126023a6892d76d603aa0f232bdd15b
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-12-19 16:18:05 -0800

    add error type to not found page to satisfy ts

--- src/views/ErrorPage/NotFoundPage.tsx
+++ src/views/ErrorPage/NotFoundPage.tsx
@@ -1,16 +1,23 @@
 import { useRouteError } from 'react-router-dom'
 
+type TError = {
+    statusText: string
+    message: string
+}
+
 export default function NotFoundPage() {
-    const error: Record<string, string> | unknown = useRouteError()
+    const error = useRouteError() as TError
     console.error(error)
 
     return (
         <div id="error-page">
             <h1>Oops!</h1>
             <p>Sorry, an unexpected error has occurred.</p>
-            <p>
-                <i>{error.statusText || error.message}</i>
-            </p>
+            {error && (
+                <p>
+                    <i>{error?.statusText || error?.message}</i>
+                </p>
+            )}
         </div>
     )
 }
