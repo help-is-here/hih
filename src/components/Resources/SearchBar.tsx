70a217ba5fbe83a0febe022ad86a56fb39eb5eaf
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-12-20 10:26:31 -0800

    Delete unused useState update methods

--- src/components/Resources/SearchBar.tsx
+++ src/components/Resources/SearchBar.tsx
@@ -1,16 +1,14 @@
-// TODO: Implement search bar
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import client from '@/database/client'
 import { useEffect, useState } from 'react'
 import { ReactSearchAutocomplete } from 'react-search-autocomplete'
 import '../../index.css'
 
 export const SearchBar = () => {
-    const [searchQuery, setSearchQuery] = useState<string>('')
+    const [searchQuery] = useState<string>('')
 
     // TODO: Define type
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const [items, setItems] = useState<any>([])
+    const [items] = useState<any>([])
     const resourcesQuery = client.from('resources').select()
 
     useEffect(() => {
