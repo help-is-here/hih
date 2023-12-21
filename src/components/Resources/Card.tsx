3bbbead77e19635e2bd0844fd0fba33a895c90c9
Xue <xue.contact@gmail.com>
2023-12-21 10:13:23 -0800

    Change margin on masonry

--- src/components/Resources/Card.tsx
+++ src/components/Resources/Card.tsx
@@ -14,7 +14,7 @@ export const Card = (props: {
     }
     return (
         <div
-            className="m-4 rounded border-2 border-solid border-white bg-white p-4 text-slate-900 drop-shadow-xl hover:border-orange-300 md:break-inside-avoid md:break-inside-avoid-column"
+            className="m-4 rounded border-2 border-solid border-white bg-white p-4 text-slate-900 drop-shadow-xl hover:border-orange-300 md:m-0 md:mb-4 md:break-inside-avoid-column"
             onClick={handleClick}
         >
             <Hearted number={numHearted} />
