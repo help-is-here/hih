ba0054780742b788d034f37def4f54eb2ad6e450
Joshua Wu <Joshua.wu.0910@gmail.com>
2023-12-22 09:23:36 -0800

    Add more eslint/ts enforcement

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,10 +10,42 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     plugins: ['react-refresh'],
     rules: {
+        'prettier/prettier': 'error',
         'react-refresh/only-export-components': [
             'warn',
             { allowConstantExport: true },
         ],
+        'react-refresh/only-export-components': 'warn',
         '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/naming-convention': [
+            'error',
+            {
+                selector: 'interface',
+                format: ['PascalCase'],
+                prefix: ['I'],
+            },
+            {
+                selector: ['typeAlias'],
+                format: ['PascalCase'],
+                prefix: ['T'],
+            },
+            {
+                selector: ['enum'],
+                format: ['PascalCase'],
+                prefix: ['E'],
+            },
+        ],
+        '@typescript-eslint/prefer-ts-expect-error': 'warn',
+        '@typescript-eslint/no-unused-vars': 'error',
+        '@typescript-eslint/ban-ts-comment': [
+            'warn',
+            {
+                'ts-expect-error': 'allow-with-description',
+            },
+        ],
+        '@typescript-eslint/no-unsafe-assignment': 'warn',
+        curly: 'error',
+        'no-console': ['error', { allow: ['error', 'warn'] }],
+        eqeqeq: 2,
     },
 }
