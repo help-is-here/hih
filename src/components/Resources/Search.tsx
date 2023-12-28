8ad899c7f3fa08422e680d2a379416357afd2194
Abi <abigail@abigailhendrick.com>
2023-12-27 20:30:42 -0500

    Added search for tags

--- src/components/Resources/Search.tsx
+++ src/components/Resources/Search.tsx
@@ -1,17 +1,26 @@
-export const Search = () => {
+import { useState } from 'react'
+import { FaSearch } from 'react-icons/fa'
+
+type TSearch = {
+    onSearch: (val: string) => void
+}
+export const Search = ({ onSearch }: TSearch) => {
+    const [search, setSearch] = useState('')
+
     return (
-        <div className="relative">
+        <div className="relative flex items-center">
             <input
                 type="text"
                 placeholder="search"
-                className="w-full rounded-lg border-none py-2 pl-10 pr-4"
+                className="w-full rounded-lg border-none py-2 ps-8"
+                value={search}
+                onChange={(e) => {
+                    setSearch(e.target.value)
+                    onSearch(e.target.value)
+                }}
             />
-            <div
-                className="pointer-events-none absolute inset-y-0 left-0
-                    flex items-center
-                    pl-3"
-            >
-                <i className="fa fa-search text-gray-400"></i>
+            <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center px-2">
+                <FaSearch />
             </div>
         </div>
     )
--- src/components/Resources/SidebarDesktop.tsx
+++ src/components/Resources/SidebarDesktop.tsx
@@ -1,5 +1,4 @@
 import { FilterButtons } from '@/components/Resources/FilterButtons.tsx'
-import { Search } from '@/components/Resources/Search.tsx'
 import TagFilters from './TagFilters'
 
 export const SidebarDesktop = (props: {
@@ -12,13 +11,12 @@ export const SidebarDesktop = (props: {
         <div
             className={`w-[300px] bg-gray-100 p-3 ${className} h-full rounded`}
         >
-            <Search />
             <FilterButtons
                 onHeartedFilter={(filterPayload) =>
                     onHeartedFilter(filterPayload)
                 }
             />
-            <div className="my-3 font-bold text-slate-900">Filter Tags</div>
+            <div className="mt-3 font-bold text-slate-900">Filter Tags</div>
             <TagFilters onFilter={(filters) => onTagsFilter(filters)} />
         </div>
     )
