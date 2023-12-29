5f9ea2059d508affc950df70f18e524734040eb8
Abi <abigail@abigailhendrick.com>
2023-12-29 14:15:30 -0500

    added more prompts

--- src/data/prompts.tsx
+++ src/data/prompts.tsx
@@ -1,13 +1,33 @@
 export const prevention = [
     'I am feeling a little down, do you have time to chat?',
-    "Can you call right now? I'm struggling a little bit.",
-    "What are you up to? There's some stuff going on and I just need to talk",
+    'I have some things I need to talk about, can we talk later?',
+    "I'd love to catch up, do you have time to talk soon?",
+    "I've been really tired lately, do you have any advice?",
+    "Can you call sometime soon? I'd love to catch up.",
+    'What have you been up to? I would like to talk sometime.',
+    "I'm hoping to catch up with friends more often, are you available sometime?",
+    'I kinda need a friend. Can we chat soon?',
+    'Would you be interested in catching up sometime?',
+    "How have you been? I'd really like to catch up.",
 ]
 
 export const precaution = [
     'I need some support, can you text with me for a little bit?',
+    "Can you call right now? I'm struggling a little bit.",
+    "What are you up to? There's some stuff going on and I just need to talk",
+    'I just need to talk to someone. Are you available?',
+    'I need a friend today. Would you be free to chat?',
+    'I feel really anxious, can we text for a little while?',
+    'Is this a good time to call? I just need to talk a little.',
+    "I'd really like to keep up more. Can we chat soon?",
 ]
 
 export const predicament = [
     'I am really struggling today. Can you talk me through some things?',
+    "I'm feeling a little scared and I need some support. Are you available?",
+    'Life has just really been tough lately. Can we talk soon?',
+    "I feel like I can't handle things, would you be willing to call me?",
+    "I've been feeling really alone lately. I could really use a friend.",
+    "Are you free right now? I'm really hurting today and I need to talk.",
+    'Can we call right now? I really need someone to talk with.',
 ]
